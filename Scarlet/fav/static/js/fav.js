e98cc02390b9f4c649d467c4988ce6999735228c
Ahmed Etefy <ahmed.etefy12@gmail.com>
2015-10-05 16:18:06 +0200

    Pushed Settings Configurations for like unlike vote unvote ...

--- Scarlet/fav/static/js/fav.js
+++ Scarlet/fav/static/js/fav.js
@@ -1,14 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function(json){
 	/* ajax function responsible for altering value of submit button and 
 	altering favorite count */
 	var fav_count = parseInt($("input[name=fav_count]").val(),10);
+	var positive_part = $("input[name=positive_part]").val();
+	var negative_part = $("input[name=negative_part]").val();
 	$(".fav-form").submit(function(event){
 	    	event.preventDefault();
             var fav_value = $("input[type=submit]").val();
             var form = $(this);
            	var data =  new FormData(form.get(0));
-           	if(fav_value == "Favorite") {
-           		data.append('fav_value','favorite')
+           	if(fav_value == positive_part) {
+           		data.append('fav_value', positive_part)
            	}
            	else{
 
@@ -27,13 +29,13 @@ $(document).ready(function(){
 	                }
 		            	else {
 		            	$(".fav_count_div").empty();
-		            	if(fav_value == "Favorite") {
+		            	if(fav_value == json["positive_part"]) {
 		            		fav_count = fav_count+1;
-		                    $("input[type=submit]").val("Unfavorite");
+		                    $("input[type=submit]").val(json["negative_part"]);
 		                  }
 		                  else{
 		                  	fav_count = fav_count-1;
-                            $("input[type=submit]").val("Favorite");
+                            $("input[type=submit]").val(json["positive_part"]);
 		                  }
 		                $("input[name=csrfmiddlewaretoken]").val(json['csrf'])
 		                $("input[name=fav_count]").val(fav_count);
