dc2e81867a4b46a52736802744de5f9770c0aeea
Ahmed Etefy <ahmed.etefy12@gmail.com>
2016-01-11 15:18:07 +0200

    Push Package

---
+++ django-scarlet/build/lib.linux-x86_64-2.7/fav/views.py
@@ -0,0 +1,66 @@
+from .models import Favorite
+from .forms import FavoriteForm
+from django.contrib.contenttypes.models import ContentType
+from django.http import JsonResponse
+from django.views.generic.edit import FormView
+from django.middleware.csrf import get_token
+from django.conf import settings
+
+
+class FavAlterView(FormView):
+
+    """
+    Enables authenticated users to Favorite/Unfavorite objects.
+
+    """
+
+    form_class = FavoriteForm
+    model = Favorite
+    template_name = 'fav/fav_form.html'
+
+    def form_valid(self, form):
+        fav_value = self.request.POST['fav_value']
+        csrf_token_value = get_token(self.request)
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        try:
+            content_type = ContentType.objects.get(
+                app_label=self.request.POST['app_name'],
+                model=self.request.POST['model'].lower())
+            model_object = content_type.get_object_for_this_type(
+                id=self.request.POST['model_id'])
+            if fav_value == settings.POSITIVE_NOTATION:
+                fav = form.save(commit=False)
+                fav.content_object = model_object
+                if self.request.user.is_authenticated():
+                    fav.save()
+                else:
+                    if settings.ALLOW_ANONYMOUS == "TRUE":
+                        fav.cookie = self.request.session.session_key
+                        fav.save()
+                    else:
+                        return JsonResponse({
+                            'success': 0,
+                            'error': "You have to sign in "})
+                Favorite.objects.get(id=fav.id)
+            else:
+                if self.request.user.is_authenticated():
+                    Favorite.objects.get(
+                        object_id=model_object.id,
+                        user=self.request.user,
+                        content_type=content_type).delete()
+                elif settings.ALLOW_ANONYMOUS == "TRUE":
+                    Favorite.objects.get(
+                        object_id=model_object.id,
+                        cookie=self.request.session.session_key,
+                        content_type=content_type).delete()
+        except:
+            return JsonResponse({
+                'success': 0,
+                'error': "You have to sign in "})
+        return JsonResponse({"csrf": csrf_token_value})
+
+    def form_invalid(self, form):
+        return JsonResponse({
+            'success': 0,
+            'error': form.errors})
